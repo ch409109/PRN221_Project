0df35d6b2df4d67653df2c0ead6581566aaa0a08
Tungnvhe <viettungnguyen2010@gmail.com>
2024-10-25 08:43:06 +0700

    news

--- BookingTicketOnline/Pages/News/ListNews.cshtml.cs
+++ BookingTicketOnline/Pages/News/ListNews.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BookingTicketOnline.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookingTicketOnline.Pages.News
 {
     public class ListNewsModel : PageModel
     {
-        public void OnGet()
+        private readonly PRN221_FinalProjectContext _context;
+
+        public ListNewsModel(PRN221_FinalProjectContext context)
         {
+            _context = context;
+        }
+
+        public List<Models.News> NewsList { get; set; }
+
+        public string LimitWords(string content, int wordLimit)
+        {
+            if (string.IsNullOrEmpty(content)) return content;
+
+            var words = content.Split(' ');
+            if (words.Length <= wordLimit) return content;
+
+            return string.Join(' ', words.Take(wordLimit)) + "...";
+        }
+
+
+        public async Task OnGetAsync()
+        {
+            NewsList = await _context.News.OrderByDescending(n => n.CreateAt).ToListAsync();
         }
     }
 }
+
--- BookingTicketOnline/Pages/News/NewsDetail.cshtml.cs
+++ BookingTicketOnline/Pages/News/NewsDetail.cshtml.cs
@@ -1,12 +1,34 @@
+using BookingTicketOnline.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
 
 namespace BookingTicketOnline.Pages.News
 {
     public class NewsDetailModel : PageModel
     {
-        public void OnGet()
+        private readonly PRN221_FinalProjectContext _context;
+
+        // Declare a public News property
+        public BookingTicketOnline.Models.News News { get; set; }
+
+        public NewsDetailModel(PRN221_FinalProjectContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            // Fetch the news item by ID from the database
+            News = await _context.News.FindAsync(id);
+
+            // Handle case where news item is not found
+            if (News == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
         }
     }
 }
