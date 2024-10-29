973cc8fe87f0b0842c3f146aff719fcc9ff46863
Tuanhai13112k3 <Tuanhai13112k3@gmail.com>
2024-10-30 03:47:33 +0700

    CompleteManageSeat

---
+++ BookingTicketOnline/Pages/Seat/AddSeat.cshtml.cs
@@ -0,0 +1,45 @@
+using BookingTicketOnline.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingTicketOnline.Pages.Seat
+{
+    public class AddSeatModel : PageModel
+    {
+		private readonly PRN221_FinalProjectContext _context;
+		public AddSeatModel(PRN221_FinalProjectContext context)
+		{
+			_context = context;
+		}
+		[BindProperty]
+		public List<Models.Row> Rows { get; set; }
+		[BindProperty]
+		public Models.Seat Seat { get; set; } = new Models.Seat();
+		public async Task<IActionResult> OnGetAsync()
+		{
+			var roomID = HttpContext.Session.GetInt32("RoomID");
+			Rows = await _context.Rows.Include(r => r.Seats).Where(r => r.RoomId == roomID).ToListAsync();
+			return Page();
+		}
+		public async Task<IActionResult> OnPostAsync()
+		{
+			var roomId = HttpContext.Session.GetInt32("RoomID");
+			var seatNumber = Request.Form["SeatNumber"];
+			var selectedRow = await _context.Rows.FindAsync(Seat.RowId);
+			Seat.SeatName = selectedRow.RowName + seatNumber;
+
+			var seat = await _context.Seats.FirstOrDefaultAsync(r => r.SeatName == Seat.SeatName);
+			if (seat == null)
+			{
+				await _context.Seats.AddAsync(Seat);
+				await _context.SaveChangesAsync();
+			}
+            else
+            {
+                return NotFound();
+            }
+            return RedirectToPage("/Seat/ManageSeats");
+		}
+	}
+}
--- BookingTicketOnline/Pages/Seat/ModifySeat.cshtml.cs
+++ BookingTicketOnline/Pages/Seat/ModifySeat.cshtml.cs
@@ -5,55 +5,36 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BookingTicketOnline.Pages.Seat
 {
-    public class ModifySeatModel : PageModel
-    {
-        private readonly PRN221_FinalProjectContext _context;
-        public ModifySeatModel(PRN221_FinalProjectContext context)
-        {
-            _context = context;
-        }
-        [BindProperty]
-        public List<Models.Seat> Seats { get; set; }
-        public List<Models.Row> Rows { get; set; }
-        [BindProperty]
-        public bool isEditing { get; set; }
-        public async Task<IActionResult> OnGetAsync(string action)
-        {
-            var roomID = 1;
-            isEditing = action == "Modify" ? true : false;
-            if (isEditing)
-            {
-                Seats = await _context.Seats.Include(s => s.Row).Where(s => s.Row.RoomId == roomID).ToListAsync();
-            }
-            else
-            {
+	public class ModifySeatModel : PageModel
+	{
+		private readonly PRN221_FinalProjectContext _context;
+		public ModifySeatModel(PRN221_FinalProjectContext context)
+		{
+			_context = context;
+		}
+		[BindProperty]
+		public List<Models.Seat> Seats { get; set; }
+		[BindProperty]
+		public Models.Seat Seat { get; set; } = new Models.Seat();
+		public async Task<IActionResult> OnGetAsync()
+		{
+			var roomID = HttpContext.Session.GetInt32("RoomID");
+			Seats = await _context.Seats.Include(s => s.Row).Where(s => s.Row.RoomId == roomID).ToListAsync();
+			return Page();
+		}
 
-            }
-
-
-            return Page();
-        }
-
-        public async Task<IActionResult> OnPostSaveAsync()
-        {
-            //if (!ModelState.IsValid)
-            //{
-            //    Rows = await _context.Rows.ToListAsync();
-            //    return Page();
-            //}
-
-            //if (Seat.Id > 0)
-            //{
-            //    _context.Seats.Update(Seat);
-            //}
-            //else
-            //{
-            //    _context.Seats.Add(Seat);
-            //}
-
-            //await _context.SaveChangesAsync();
-            return RedirectToPage("/Seat/ManageSeats");
-        }
-    }
+		public async Task<IActionResult> OnPostAsync()
+		{
+			var roomID = HttpContext.Session.GetInt32("RoomID");
+			var seat = _context.Seats.Include(s => s.Row).FirstOrDefault(s => s.Row.RoomId == roomID && s.SeatName == Seat.SeatName);
+			if (seat != null)
+			{
+				seat.Status = Seat.Status;
+				_context.Seats.Update(seat);
+			}
+			await _context.SaveChangesAsync();
+			return RedirectToPage("/Seat/ManageSeats");
+		}
+	}
 }
 
