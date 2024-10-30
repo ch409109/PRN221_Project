5d3055dc995927b2c927526b397c8a31e636f874
tuanmin1806 <trinhngoctuan2002@gmail.com>
2024-10-30 08:22:05 +0700

    movie detail

--- BookingTicketOnline/Pages/Movie/MovieDetails.cshtml.cs
+++ BookingTicketOnline/Pages/Movie/MovieDetails.cshtml.cs
@@ -9,16 +9,18 @@ namespace BookingTicketOnline.Pages.Movie
     {
         private readonly PRN221_FinalProjectContext _context;
 
-        public Models.Movie Movie { get; set; } 
+        public Models.Movie? Movie { get; set; }
 
         public MovieDetailsModel(PRN221_FinalProjectContext context)
         {
             _context = context;
         }
 
-        public async Task OnGetAsync(int id) //
+        public async Task OnGetAsync(int id)
         {
-            Movie = await _context.Movies.FirstOrDefaultAsync(m => m.Id == id);
+            Movie = await _context.Movies
+            .Include(m => m.Category)
+            .FirstOrDefaultAsync(m => m.Id == id);
         }
     }
 }
