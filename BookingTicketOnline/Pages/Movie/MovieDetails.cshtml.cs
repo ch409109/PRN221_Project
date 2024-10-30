cd4db3f7c2f6edf410ccbed2b78194fb577a628f
Hoang Thanh Cong <ch409109@gmail.com>
2024-10-30 10:05:51 +0700

    feat: update Trailer for movie

--- BookingTicketOnline/Pages/Movie/MovieDetails.cshtml.cs
+++ BookingTicketOnline/Pages/Movie/MovieDetails.cshtml.cs
@@ -22,5 +22,26 @@ namespace BookingTicketOnline.Pages.Movie
             .Include(m => m.Category)
             .FirstOrDefaultAsync(m => m.Id == id);
         }
+
+        public string GetEmbedUrl(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+                return "";
+
+            // Xử lý URL dạng youtube.com/watch?v=
+            if (url.Contains("watch?v="))
+            {
+                var videoId = url.Split("watch?v=")[1];
+                return $"https://www.youtube.com/embed/{videoId}";
+            }
+            // Xử lý URL dạng youtu.be/
+            else if (url.Contains("youtu.be/"))
+            {
+                var videoId = url.Split("youtu.be/")[1];
+                return $"https://www.youtube.com/embed/{videoId}";
+            }
+
+            return url; // Trả về nguyên URL nếu không match các pattern trên
+        }
     }
 }
