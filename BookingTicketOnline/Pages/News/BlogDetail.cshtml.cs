40f33ef369a6e7378e4253b0767d891ea77c3864
tuanmin1806 <trinhngoctuan2002@gmail.com>
2024-11-09 10:14:42 +0700

    commit

--- BookingTicketOnline/Pages/News/BlogDetail.cshtml.cs
+++ BookingTicketOnline/Pages/News/BlogDetail.cshtml.cs
@@ -25,8 +25,18 @@ namespace BookingTicketOnline.Pages.News
 
         public async Task<IActionResult> OnGetAsync(int id)
         {
-            News = await _context.News.Include(n => n.CreateByNavigation).OrderByDescending(n => n.CreateAt).ToListAsync();
-            news = await _context.News.AsNoTracking().Include(n => n.CreateByNavigation).FirstOrDefaultAsync(n => n.Id == id);
+            // Load the latest news, excluding the current news item
+            News = await _context.News
+                .Include(n => n.CreateByNavigation)
+                .OrderByDescending(n => n.CreateAt)
+                .Where(n => n.Id != id) // Exclude the current news from the list
+                .ToListAsync();
+
+            // Load the current news detail
+            news = await _context.News
+                .AsNoTracking()
+                .Include(n => n.CreateByNavigation)
+                .FirstOrDefaultAsync(n => n.Id == id);
 
             if (news == null)
             {
